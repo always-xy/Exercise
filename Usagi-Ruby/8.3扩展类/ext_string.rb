6d65391310c27df4cc9e10a6afbc551afd2aaa07
Always-YX <always_yx@outlook.com>
2017-10-20 11:10:01 +0800

    20171020

--- Usagi-Ruby/8.3扩展类/ext_string.rb
+++ Usagi-Ruby/8.3扩展类/ext_string.rb
@@ -7,3 +7,20 @@ end
 
 str = "Just Another Ruby Newbie"
 p str.count_word            #=> 4
+
+
+# (1) 在默认无参传入的时候 ，是以空格为间隔 ，获得数组
+# [1] pry(main)> " now's the time".split
+# => ["now's", "the", "time"]
+
+# (2) 如果接受一个字符参数 ，那么会按照这个字符参数进行分割变成数组 
+# [2] pry(main)> "mellow yellow".split("ello")
+# => ["m", "w y", "w"]
+
+# (3)如果接受的是两个参数 ，后面的参数是用来确定分割数组里面元素的个数，如果在按规则分割的时候 剩下多余的就变成一个元素
+# [3] pry(main)> "mellow,yellow".split(//,4)
+# => ["m", "e", "l", "low,yellow"]
+
+# (4)当然split 参数 也接受正则表达式，如下 
+# [4] pry(main)> "hi mom".split(%r{\s*})
+# => ["h", "i", "m", "o", "m"]
