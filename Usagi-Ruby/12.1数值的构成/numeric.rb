3233a62b0a21be3dfc649fa9e098c87418ab4f5e
Always-YX <always_yx@outlook.com>
2018-01-09 16:25:05 +0800

    2018.01.09

---
+++ Usagi-Ruby/12.1数值的构成/numeric.rb
@@ -0,0 +1,14 @@
+数值中有很多类型，这些数值被定义成Numeric的子类.
+
+Numeric ------>Integer------->Fixnum
+ (数值)	   |	(整数)	  |  (普通整数)
+ 		   |              --->Bignum
+ 		   |				  (大整数)
+ 		   --->Float
+ 		   | (浮点小数)
+ 		   |
+ 		   --->Rational
+ 		   |   (有理数)
+ 		   |
+ 		   --->Complex
+ 		   		(复数)
