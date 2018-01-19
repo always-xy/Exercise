791277ba9b3adfc5a1717d2d5e2183a88562f7d1
Always-YX <always_yx@outlook.com>
2018-01-19 16:21:37 +0800

    2018.01.19

---
+++ Usagi-Ruby/14.0练习题/capitlize.rb
@@ -0,0 +1,12 @@
+str = "Ruby is an object oriented programming language"
+ary = str.split
+cap_ary = ary.collect{|word| word.capitalize }
+
+str = ""
+cap_ary.each do |s|
+  str << s+" "
+end
+p str
+
+## 另一种解法
+p cap_ary.join(" ")
