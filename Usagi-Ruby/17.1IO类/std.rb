25ea369236f2824f333b402c419b7109d4d5c535
Always-YX <always_yx@outlook.com>
2018-01-30 16:59:16 +0800

    2018.01.29

--- Usagi-Ruby/17.1IO类/std.rb
+++ Usagi-Ruby/17.1IO类/std.rb
@@ -29,4 +29,70 @@ while line = io.gets
   #条件处理
 end
 p io.eof?
+
+while条件表达式中同时进行了变量赋值与条件判断的操作。将gets方法的返回值复制给line，并将值作为while判断。
+用each_line方法也可以到达同样的效果。
+io.each_line do |line|
+  line.chomp!
+  #条件处理
+end
+用readslines方法可以一次性读取所有数据，并返回将每行数据作为元素封装的数组。
+ary = io.readlines
+ary.each_line do |line|
+  line.chomp!
+  #条件判断
+end
+
+gets、each_line方法会逐行读取数据，并且自动记录行数，通过lineno方法获取，此外lineno=方法可以改变这个值
+，但是值的改变并不会对文件指针有影响。
+
+#读取文本并 在文件首行添加行号
+$stdin.each_line do |line|
+  printf("%3d %s", $stdin.lineno, line)
+end
+
+#io.each_char 逐个读取IO中的数据并执行块，将得到的字符（String对象）作为块变量传递。
+io.each_line do |ch|
+  #对line进行操作
+end
+
+#io.each_byte对逐个字节进行操作,并启动块。
+#io.getc值读取io中的一个字符，根据文件编码不同，有时一个字符会由多个字节组成，但这个方法只会读取一个字符，
+然后返回字符串对象，数据全部读取完成后再读取时会返回nil。
+while ch = io.getc
+  #对ch进行操作
+end
+
+#io.ungetc 将参数ch指定的字符返回到io的输入缓冲中。
+#hello.txt中内容为Hello,Ruby.\n
+File.open("hello.txt") do |io|
+  p io.getc
+  io.ungetc
+  p io.gets
+end
+
+#io.getbyte 只读取io中的一个字节，返回得到的字符转换为ASCII码后的整数对象。数据全部读取完成后会返回nil。
+
+#io.ungetbyte(byte) 将参数byte指定的一个字节退回到输入缓冲中，参数为整数时，将该整数除以256后的余数作为ASCII码
+字符返回一个字节，参数为字符串时，只返回字符串的第一个字节。
+
+#io.read(size) 读取参数size指定的大小的数据，不指定大小时，则一次性读取全部数据并返回。
+File.open("hello.txt") do |io|
+  p io.read(5)
+  p io.read
+end
+
+#输出操作
+#io.puts(str0,str1^^^)对字符串末尾添加换行符后输出,指定多个参数时，会分别添加换行符。如果参数为String,
+会自动调用to_s方法，将其转换为字符串后再输出。
+#$stdout.puts "foo","bar","baz"
+
+#io.puts(ch)输出参数ch指定的字符编码对应的字符，参数为字符串时输出首字符。
+#$stdout.putc(82)
+#$stdout.putc("Ruby")
+#$stdout.puts("\n")
+
+#io.print(str0,str1^^^)
+输出参数指定的字符串，参数可指定多个字符串，参数为String以外的对象时对象会自动为其转换类型为字符串。
+
 =end
