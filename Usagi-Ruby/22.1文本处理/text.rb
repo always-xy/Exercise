3944cdadab836d868f7c5004bda5c96b1fcbea54
Always-YX <always_yx@outlook.com>
2018-02-08 17:04:56 +0800

    2018.02.08

--- Usagi-Ruby/22.1文本处理/text.rb
+++ Usagi-Ruby/22.1文本处理/text.rb
@@ -13,3 +13,21 @@ File.open(filename, "w") do |f|
   f.write text
   #f.write NKF.nkf("-s", text)
 end
+
+htmlfile = "cathedral.html"
+textfile = "cathedral.txt"
+
+html = File.read(htmlfile)
+
+File.open(textfile, "w") do |f|
+  in_header = true
+  html.each_line do |line|
+	if in_header && /<a name="1">/ !~ line
+  	  next
+  	else
+  	  in_header = false
+  	end
+  	break if /<a name="version">/ =~ line
+  	f.write line
+  end
+end
